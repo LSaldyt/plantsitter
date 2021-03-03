0699de9e43e24e88f14abdd67e46b9e607214ad0
LSaldyt <lucassaldyt@gmail.com>
2021-03-02 23:20:30 -0600

    Merge stashed changes

--- sitter/sitter.py
+++ sitter/sitter.py
@@ -11,15 +11,16 @@ from utils.influx  import SeriesDatabase
 
 import asyncio
 
-SPEC    = dict(probe=6)
+PROBES = [6, 13, 19, 26, 16, 21, 12, 5]
+SPEC    = {f'moisture_{i}' : p for i, p in enumerate(PROBES)}
+SPEC.update(dict(light=25, humidity=24))
 SENSORS = initialize_sensors(SPEC)
 LOCK    = asyncio.Lock()
 
-app  = Quart(__name__)
 conn = SeriesDatabase()
 
-interval = 5
-
+database_interval = 100
+system_interval   = 10
 @app.websocket('/data')
 async def datasocket():
     count = 0
@@ -28,14 +29,16 @@ async def datasocket():
         count += 1
         async with LOCK:
             plantdata = {k : v.value for k, v in SENSORS.items()}
+        if count % system_interval == 0:
             plantdata.update(snapshot())
-            buff.append(plantdata)
-            plantdata['timestamp'] = time()
-            await websocket.send_json(plantdata)
-        if count % interval == 0:
-            pprint(buff)
+        plantdata['timestamp'] = time()
+        # time=datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        buff.append(plantdata)
+        await websocket.send_json(plantdata)
+        if count % database_interval == 0:
             conn.insert(buff)
             del buff[:]
+        print(f'{count}\r', end='')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
