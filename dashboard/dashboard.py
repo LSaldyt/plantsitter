aa6a8bbc5ecb44ff6e41b344abff0704c6cc1f8e
LSaldyt <lucassaldyt@gmail.com>
2021-03-20 14:04:02 -0500

    Separate layout & dashboard logic

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -20,6 +20,8 @@ import json
 TELEM = ['light', 'humidity' ]
 LATENCY_THRESHOLD = 0.05
 
+from layout import create_elements
+
 class PlantDash:
     def __init__(self, app):
         self.config = Config()
@@ -31,133 +33,7 @@ class PlantDash:
         self.pcount = 0
         self.start = datetime.now()
 
-        elements = [html.Div(
-                        [
-                            html.Div(
-                                [
-                                    html.Img(
-                                        src=app.get_asset_url('asu_logo_alt.png'),
-                                        id="plotly-image",
-                                        style={
-                                            "height": "100px",
-                                            "width": "auto",
-                                            "margin-bottom": "25px",
-                                        },
-                                    )
-                                ],
-                                className="one-third column",
-                            ),
-                            html.Div(
-                                [
-                                    html.Div(
-                                        [
-                                            html.H3(
-                                                'PlantSitter',
-                                                style={"margin-bottom": "0px"},
-                                            ),
-                                            html.H5(
-                                                'Raspberry-Pi Based Live Agriculture Monitoring', style={"margin-top": "0px"}
-                                            ),
-                                            html.H6(
-                                                'By Lucas Saldyt', style={"margin-top": "0px"}
-                                            ),
-
-                                        ]
-                                    )
-                                ],
-                                className="one-half column",
-                                id="title",
-                            ),
-                            html.Div(
-                                [
-                                ],
-                                className="one-third column",
-                                id="button",
-                            ),
-                        ],
-                        id="header",
-                        className="row flex-display",
-                        style={"margin-bottom": "25px"},
-                    ),
-                    html.Div(
-                        [
-                            html.Div(
-                                [
-                                    html.H4('Settings'),
-                                    dcc.Dropdown(
-                                        id='plant_select',
-                                        options=[dict(label='Agave',    value='Agave'),
-                                                 dict(label='Rosemary', value='Rosemary')],
-                                        multi=True,
-                                        value=['Agave', 'Rosemary'],
-                                        className="dcc_control",
-                                    ),
-                                    dcc.Dropdown(
-                                        id='actuator',
-                                        options=[dict(label='Pump', value='main'),
-                                                 dict(label='L/R',  value='horizontal'),
-                                                 dict(label='U/D',  value='vertical'),
-                                                 ],
-                                        value='main',
-                                        className="dcc_control",
-                                    ),
-                                    html.Div(dcc.Input(id='input-on-submit', type='number', value=1.0)),
-                                    html.Button('Submit', id='command', n_clicks=0)
-                                ],
-                                className="pretty_container four columns",
-                                id="cross-filter-options",
-                            ),
-                            html.Div(
-                                [
-                                    html.Div(
-                                        [
-                                            html.Div(
-                                                [html.H6('Latency', id='latency_text')],
-                                                id='latency',
-                                                className="mini_container",
-                                            ),
-                                            html.Div(
-                                                [html.H6('Receive Rate', id='rrate_text')],
-                                                id='rrate',
-                                                className="mini_container",
-                                            ),
-                                            html.Div(
-                                                ['Plotting Rate', html.H6(id='prate_text')],
-                                                id='prate',
-                                                className="mini_container",
-                                            ),
-                                            html.Div(
-                                                ['Humidity', html.H6(id='humidity_text')],
-                                                id='humidity',
-                                                className="mini_container",
-                                            ),
-                                            html.Div(
-                                                ['Light', html.H6(id='light_text')],
-                                                id='light',
-                                                className="mini_container",
-                                            ),
-                                        ],
-                                        id="info-container",
-                                        className="row container-display",
-                                    ),
-                                    html.Div(
-                                        [dcc.Graph(id="count_graph")],
-                                        id="countGraphContainer",
-                                        className="pretty_container",
-                                    ),
-                                ],
-                                id="right-column",
-                                className="eight columns",
-                            ),
-                        ],
-                        className="row flex-display",
-                    ),
-                    html.Br(),
-                    ]
-        for k in TELEM:
-            elements.append(html.Div(f'{k.title()} Measurement'))
-            elements.append(dcc.Graph(id=f'{k}_graph'))
-        self.app.layout = html.Div(elements)
+        self.app.layout = create_elements(app, TELEM)
 
         @app.callback(None, [dash_devices.dependencies.Input('command', 'n_clicks')],
                             [dash_devices.dependencies.State('input-on-submit', 'value'),
@@ -170,6 +46,7 @@ class PlantDash:
         @self.app.callback_connect
         def func(client, connect):
             print(client, connect, len(app.clients))
+            self.app.push_mods({'plant_map' : {'figure' : px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])}})
             if connect and len(app.clients) == 1:
                 self.start = datetime.now()
                 self.communication_loop()
