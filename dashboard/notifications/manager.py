34ecd3c948ea052e87ec50d8f73b651ff3cd0934
LSaldyt <lucassaldyt@gmail.com>
2021-08-02 13:31:06 -0700

    Update packages & ignore creds if not present

--- dashboard/notifications/manager.py
+++ dashboard/notifications/manager.py
@@ -4,5 +4,11 @@ from .email import EmailClient
 
 class NotificationManager:
     def __init__(self):
-        self.text  = TextClient()
-        self.email = EmailClient()
+        try:
+            self.text  = TextClient()
+        except:
+            print('Could not create Twilio text client. Check config.json')
+        try:
+            self.email = EmailClient()
+        except:
+            print('Could not create Google Email client. Check config.json, credentials.json, and token.json.')
